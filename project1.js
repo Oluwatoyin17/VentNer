a577c274a29151a9b05842ee3d30d18e9a0e18f5
DMFletcher <thanatos.02@gmail.com>
2018-03-25 18:04:56 -0500

    final commit, cleaned code

--- project1.js
+++ project1.js
@@ -14,6 +14,139 @@ window.onclick = function (event) {
 		modal.style.display = "none";
 	}
 }
+
+
+
+$(document).ready(function () {
+	// type_holder
+	// <div><label><input type="checkbox" class="types" value="mosque" />Mosque</label></div>
+
+	var types = ['amusement_park', 'aquarium', 'art_gallery', 'bar', 'campground', 'church', 'hindu_temple', 'lodging', 'mosque', 'movie_theater', 'museum', 'night_club', 'park', 'restaurant', 'school', 'stadium', 'synagogue'];
+	var html = '';
+
+	$.each(types, function (index, value) {
+		var name = value.replace(/_/g, " ");
+		html += '<div><label><input type="checkbox" class="types" value="' + value + '" />' + capitalizeFirstLetter(name) + '</label></div>';
+	});
+
+	$('#type_holder').html(html);
+});
+
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+var map;
+var infowindow;
+var autocomplete;
+var countryRestrict = { 'country': 'in' };
+var selectedTypes = [];
+
+function initialize() {
+	autocomplete = new google.maps.places.Autocomplete((document.getElementById('address')), {
+		types: ['(regions)'],
+		// componentRestrictions: countryRestrict
+	});
+
+	var houston = new google.maps.LatLng(29.8159954, -95.9617495);
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: houston,
+		zoom: 10
+	});
+}
+
+function renderMap() {
+	// Get the user defined values
+	var address = document.getElementById('address').value;
+	var radius = parseInt(document.getElementById('radius').value) * 1000;
+
+	// get the selected type
+	selectedTypes = [];
+	$('.types').each(function () {
+		if ($(this).is(':checked')) {
+			selectedTypes.push($(this).val());
+		}
+	});
+
+	var geocoder = new google.maps.Geocoder();
+	var selLocLat = 0;
+	var selLocLng = 0;
+
+	geocoder.geocode({ 'address': address }, function (results, status) {
+		if (status === 'OK') {
+			//console.log(results[0].geometry.location.lat() + ' - ' + results[0].geometry.location.lng());
+
+			selLocLat = results[0].geometry.location.lat();
+			selLocLng = results[0].geometry.location.lng();
+
+			//var houston = new google.maps.LatLng(29.8159954, -95.9617495);
+
+			var houston = new google.maps.LatLng(selLocLat, selLocLng);
+
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: houston,
+				zoom: 10
+			});
+
+			//console.log(selectedTypes);
+
+			var request = {
+				location: houston,
+				//radius: 5000,
+				//types: ["atm"]
+				radius: radius,
+				types: selectedTypes
+			};
+
+			infowindow = new google.maps.InfoWindow();
+
+			var service = new google.maps.places.PlacesService(map);
+			service.nearbySearch(request, callback);
+		}
+		else {
+			alert('Geocode was not successful for the following reason: ' + status);
+		}
+	});
+}
+
+function callback(results, status) {
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < results.length; i++) {
+			createMarker(results[i], results[i].icon);
+			console.log(results[i]);
+		}
+	}
+}
+
+function createMarker(place, icon) {
+	var placeLoc = place.geometry.location;
+
+	var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location,
+		icon: {
+			url: icon,
+			scaledSize: new google.maps.Size(20, 20) // pixels
+		},
+		animation: google.maps.Animation.DROP
+	});
+
+	google.maps.event.addListener(marker, 'click', function () {
+		infowindow.setContent('Name: ' + place.name + '<br>' + 'Address: ' + place.vicinity + '<br>' + "Rating: " + place.rating);
+		infowindow.open(map, this);
+	});
+}
+
+
+
+
+
+
+
+
+
+
 //log in information put in local storage
 function localLogin() {
 	// Clear sessionStorage
@@ -24,8 +157,8 @@ function localLogin() {
 	sessionStorage.setItem("email", userLocal.email);
 	sessionStorage.setItem("pass", userLocal.password);
 }
-function amILoggedIn(){
-	if (sessionStorage){
+function amILoggedIn() {
+	if (sessionStorage) {
 		$("#login-button").html(sessionStorage.name);
 		console.log(sessionStorage.name);
 	}
@@ -61,7 +194,7 @@ $("#loginBtn").on("click", function () {
 	}
 });
 //log out
-$("#logout-button").on("click" , function(){
+$("#logout-button").on("click", function () {
 	sessionStorage.clear();
 })
 //whenever sign up button is clicked...
@@ -99,6 +232,11 @@ $("#signUp").on("click", function () {
 				password: password,
 				//dateAdded: firebase.database.serverValue.TIMESTAMP
 			});
+			userLocal = {
+				"name": userName,
+				"email": userEmail,
+				"password": password
+			}
 			localLogin();
 			amILoggedIn();
 		}
@@ -135,27 +273,14 @@ $("#submit").on("click", function (event) {
 	var longitude;
 	var latitude;
 	var apiArray = [
-		"f0236529f3a885d0f84f69ada3bd541e",
-		//"P6SP3YQDM2IL5XINGMQHQIJDYNQTZPYXJMBLMJLFTBUH4PYTMV" , 
-		//"22546c4a3636b34204b7b4c7d3d682b" ,
-		//"AIzaSyAOJy5-QBRK20FJ5SsYtPUkb1Urvyt_jy8"
-	];
+		"f0236529f3a885d0f84f69ada3bd541e"];
 	var queryArray = [
-		"https://api.openweathermap.org/data/2.5/weather?zip=" + (address) + "&units=imperial&APPID=" + apiArray[0],
-		//"https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=" + apiArray[1] , 
-		//"https://api.meetup.com/2/events?key=" + apiArray [2] + "&group_urlname=ny-tech&sign=true" , 
-		//"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=" + apiArray[1] , 
-		//"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=500&key=" + apiArray[1]
+		"https://api.openweathermap.org/data/2.5/weather?zip=" + (address) + "&units=imperial&APPID=" + apiArray[0]
 	];
 	var checkboxArray = [];
 
 
-	//for(i = 0; i < apiArray.length;i++){
-
-	//var APIKey = apiArray[i];
 	console.log(queryArray[0]);
-	//var queryURL = queryArray[i];
-	//then query Weather API for weather data (and lat/lng)
 	$.ajax({
 		url: queryArray[0],
 		method: "GET"
@@ -167,30 +292,11 @@ $("#submit").on("click", function (event) {
 		latitude = response.coord.lat;
 		console.log(latitude, longitude);
 
-		//Build array of checkbox items....
-		$('#checkboxDiv input:checked').each(function () {
-			checkboxArray.push($(this).val());
-			console.log($(this).val());
-		});
 
-		//TODO: display weather data.....
+		// display weather data.....
 		$("#weather-data").html("<div id='cTemp'>" + "Current Temperature: " + response.main.temp + " F </div>");
 		$("#weather-data").append("<div id='cCond'>" + "Current Conditions: " + response.weather["0"].main + "</div>");
 		$("#weather-data").append("<div id='humid'>" + "Humidity: " + response.main.humidity + "</div>");
-
-		//query GoogleMaps API (using LAT/LNG & checkboxArray)
-
-		// $.ajax({
-		// 	url: queryArray[1],
-		// 	method: "GET"
-		// }).then(function (response) {
-
-		// 	//TODO: get results from venue checkboxes&
-
-		// 	//TODO: display results on map and as entries
-
-		// 	console.log(response);
-		// });
 
 	});
 
